31bfa4fa3d3efbe95ebff774cb4e1c175fdd2eef
atakowiec <mat.cieszczyk@gmail.com>
2025-05-11 17:14:11 +0200

    feat: add status handler to RCCarLibrary

--- RCCarLibrary/RCCar.h
+++ RCCarLibrary/RCCar.h
@@ -10,10 +10,12 @@
 #define MAX_SPEED 251
 
 typedef void (*EventHandler)(int event, char* message);
+typedef void (*MessageStatusHandler)(bool error);
 
 void initEspNow(uint8_t *peerAddress);
 void sendEvent(int event, char* message);
 void setEventHandler(EventHandler handler);
+void setMessageStatusHandler(MessageStatusHandler handler);
 void internalHandleEvent(int event, char* message);
 
 #endif
